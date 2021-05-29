ef23c1ca67ce8ddcd5466af6830a3aa49202ed28
unknown <jigneshkumar.savaliya@cognizant.com>
2021-05-28 19:19:38 -0500

    Updates

---
+++ precache-manifest.f9fc468c98a27acaced8ffde4a795f4d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "debacd469e17c6a4f8e7398bb01ee767",
+    "url": "/react-property-calculator/index.html"
+  },
+  {
+    "revision": "00d3a5b004e4896fa9c8",
+    "url": "/react-property-calculator/static/js/2.907b6087.chunk.js"
+  },
+  {
+    "revision": "b5da303589b0c9b62da6",
+    "url": "/react-property-calculator/static/js/main.f497b865.chunk.js"
+  },
+  {
+    "revision": "2c57073ca73d3a508b53",
+    "url": "/react-property-calculator/static/js/runtime~main.c87334e8.js"
+  }
+]);
